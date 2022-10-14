299eba6c023b857eced98b1e038522d3f681a03c
iwilldaydream <khalilhamzaoui.kh@gmail.com>
2022-10-14 13:12:24 -0800

    adding functionality

--- frontEnd/src/Main.js
+++ frontEnd/src/Main.js
@@ -5,75 +5,34 @@ import bobbyficher from "./images/bobbyficher.jpeg"
 import chessForKids from "./images/chessForKids.jpg"
 import chessTournament from "./images/chessTournament.jpeg"
 class Main extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            result: 0,
+        }
 
-
-     each(coll,func)  { 
-        if (Array.isArray(coll)){
-            for (var i =0;i<coll.length;i++){
-            func(coll[i],i)
-        }
-        }
-        else {
-            for (var key in coll){
-                func(coll[key],key)
-            }
-        }
     }
-      reduce(array, f, start) { 
-           var acc = start; 
-           each(array, function(element) { 
-                 acc = f(acc, element); 
-           }); 
     
-           return acc; 
-     }
-   sum(array){
-
-        return reduce(array,function(result,e,i){
-      return  result + e
-        },0)
     
-        }
-    
-        randomfunction(){ result = sum($(".prc"))
-        $("#finished").click(function(){
-            $("#secondSpan").append(`<p>${result}</p>`)
-        })
-        }
-     sum2(){
-        var result=0
-        var k=$(".prc");
-        for (var i =0;i<k.length;i++){
-            result+=parseInt(k[i].innerHTML)
-        }
-        return result
-    }
-    function1 (){$("#finished").click(function(){
-        $("#secondSpan").append(`<p id = "cost"> your Order cost : ${sum2()} dt </p>`)
-    })
-    $("#reset").click(function(){
-        $(".p").remove()
-        $("#cost").remove()
-    })
-}
-
     render(){
         return (
             
             <main>
+            <div id ="anotherDiv">
             <div id = "containerCards">
             <div class="card">
             <img src={tactics} alt="Avatar" class = "cards" ></img>
             <div class="container">
             <h4><b>Tactics Time</b></h4>
-            <button>10 dollars</button>
+            <button onClick={()=>this.setState({ result: this.state.result + 9 })}>9 dollars</button>
+            {console.log(this.state.result)}
             </div>
             </div>
             <div class="card">
             <img src={bobbyficher} alt="Avatar" class = "cards" ></img>
             <div class="container">
             <h4><b>bobby ficher teacher chess</b></h4>
-            <button>15 dollars</button>
+            <button onClick={()=>{this.setState({ result: this.state.result + 19 })}}>19 dollars</button>
             
             </div>
             </div>
@@ -81,7 +40,7 @@ class Main extends React.Component {
             <img src={chessForKids} alt="Avatar" class = "cards" ></img>
             <div class="container">
             <h4><b>Chess for kids</b></h4>
-            <button>20 dollars</button>
+            <button onClick={()=>this.setState({ result: this.state.result + 23 })}>23 dollars</button>
             
             </div>
             </div>
@@ -89,15 +48,17 @@ class Main extends React.Component {
             <img src={chessTournament} alt="Avatar" class = "cards" ></img>
             <div class="container">
             <h4><b>Chess for kids</b></h4>
-            <button>20 dollars</button>
+            <button onClick={()=>this.setState({ result: this.state.result + 1999 })}>1999 dollars</button>
             
             </div>
             </div>
             </div>
             <div id= "bill">
                 <h2>your bill sir : </h2> 
+                <div id = "resultContainer"><h3>Your sum :! </h3><h5>{this.state.result}</h5><button id = "buy">buy</button></div>
+                
             </div>
-
+            </div>
             </main>
         )
     }
